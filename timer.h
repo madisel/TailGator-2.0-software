0152ac5a88dd24b696714c92f039168ae8d856a3
Elliot Schoenfeld <jeofner@gmail.com>
2014-03-18 10:10:17 -0400

    timer.h
    header file for timer

---
+++ timer.h
@@ -0,0 +1,9 @@
+//Header file linking to the 4 minute delay function time_Cook();
+//Add to c files by #include "timer.h"
+
+#ifndef TIMER_H_INCLUDED
+#define	TIMER_H_INCLUDED
+
+void time_Cook();
+
+#endif
